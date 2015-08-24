3f3e6a17db4182b516db0cd088f7973adb2e60a9
jfanderson <jfanderson373@gmail.com>
2015-08-24 14:28:48 -0700

    Implemented row and column conflict checks

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,31 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var board = this.rows()
+      var col = 0;
+      var row = rowIndex;
+      var pieceCount = 0;
+      while (this._isInBounds(row, col)) {
+        if (board[row][col]) {
+          pieceCount++;
+        }
+        if (pieceCount > 1) {
+          return true;
+        }
+        col++;
+      }
+
+      return false; 
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      for (var i = 0; i < this.get('n'); i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false; 
     },
 
 
@@ -94,12 +113,31 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var board = this.rows()
+      var col = colIndex;
+      var row = 0;
+      var pieceCount = 0;
+      while (this._isInBounds(row, col)) {
+        if (board[row][col]) {
+          pieceCount++;
+        }
+        if (pieceCount > 1) {
+          return true;
+        }
+        row++;
+      }
+
+      return false; 
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      for (var i = 0; i < this.get('n'); i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+      return false; 
     },
 
 
